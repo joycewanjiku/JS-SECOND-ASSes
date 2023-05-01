83f2ff751049a5021729121532b965adeb0f20a6
student <joycemumbiiwanjiku@gmail.com>
2023-05-01 09:02:28 +0300

    obje_asse

---
+++ Js_Asses.js
@@ -0,0 +1,63 @@
+// 1. Create a function called getAvailableBooks that returns an array of all available
+// books.
+function getAvailableBooks(Books){
+    let books={};
+    if(getAvailableBooks.array.forEach(books => Books(Books).push){
+        return "allvailable books"
+        
+    });
+
+}
+// 
+// 2. Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+function getBooksByAuthor(authorname){
+    let getBooksByAuthor={};
+    if(authorname in getBooksByAuthor==allbooks .push(books)
+
+      ){
+        return allbooks
+        
+    }
+
+    
+}
+console.log(getBooksByAuthor);
+// 3. Create a function addNewBook that takes a book object as an argument and adds it
+// to the library, ensuring that the new book has all required properties (title, author,
+// publicationYear, and isAvailable).
+function addNewBook(book) {
+    const Newbook = addedbook(Allstars);
+    if (Newbook in book ==addedbook(title,author,publicationYear)) {
+        
+    }
+    
+    
+}
+// 4. Create a function checkoutBook that takes a book title as an argument and changes
+// the book's isAvailable property to false. If the book is not found in the library, the
+// function should return a message indicating that the book is not available.
+function checkoutBook(booktitle) {
+    const checkoutBook={};
+    if (i in empty ==checkbooks.push(book)
+    ) {
+        
+    }
+    const[
+    
+        // { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,}
+    ]
+    
+}
+// 5. Create a function returnBook that takes a book title as an argument and changes the
+// book's isAvailable property to true. If the book is not found in the library, the function
+// should return a message indicating that the book does not belong to the library.
+function returnBook(books) {
+    let returnbook=x
+    if ( i in x ==books ) {
+        return "books does not belong to the library"
+        
+    }
+    
+}
+console.log(returnBook);
